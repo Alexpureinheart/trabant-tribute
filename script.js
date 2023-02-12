5425b71ecdb7b4b68fa2be85e1295580ec2a9b62
Alex Watts-Barnett <alexrwb@gmail.com>
2023-02-13 00:55:49 +0900

    added englarge gallery function to script and related tags

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-galleryImage = document.querySelector(".gallery-image");
+const galleryImages = document.querySelectorAll(".gallery-image");
+
+galleryImages.forEach(function (image) {
+  image.addEventListener("click", function () {
+    image.classList.toggle("enlarged");
+    image.classList.toggle("gallery-image");
+  });
+});
